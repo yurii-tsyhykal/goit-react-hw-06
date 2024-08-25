b8e6470ebe016d39e69c34f26b840aba17d69008
Yurii <y.tsyhykal@gmail.com>
2024-08-25 23:00:01 +0200

    added filterValue from redux store

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,13 +1,15 @@
-import { useDispatch } from "react-redux";
-import { changeFilter } from "../../redux/filtersSlice/filtersSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { changeFilter, selectNameFilter } from "../../redux/filtersSlice/filtersSlice";
 
 const SearchBox = () => {
   const dispatch = useDispatch();
+  const filterValue = useSelector(selectNameFilter)
   return (
     <div style={{ margin: 20 }}>
       <p>Find contacts by name</p>
       <input
         type="text"
+        value={filterValue}
         onChange={e => dispatch(changeFilter(e.target.value))}
       />
     </div>
